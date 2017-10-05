56ebf644d4d64bf3357f59830209b54b3d229294
deston-trace <destontrace@gmail.com>
2017-10-05 17:15:07 +0500

    added email confermation https://medium.com/@dkever/add-email-confirmation-to-your-rails-app-w-devise-ffcf3b9319e2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,18 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :port           => '587',
+      :authentication => :login,
+      :user_name      => 'adeelahmadawan',
+      :password       => '145512181314122114',
+      :domain         => 'gmail.com',
+      :enable_starttls_auto => true
+  }
+
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
   root 'welcome#index'
+
+  resource :users do
+    member do
+      get :confirm_email
+    end
+  end
+
 end
